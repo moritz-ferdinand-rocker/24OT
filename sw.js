6527a95efdb1bbe2bf76f916fc5abc7317a3ff59
Moritz Ferdinand Rocker <rocker.moritz@gmail.com>
2020-01-15 14:12:35 +0100

    added little service worker

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
 // Copyright Moritz Ferdinand Rocker 2020.
 const CACHE = 'cache';
-const URLS_TO_CACHE = 'index.html';
-self.oninstall = (e) => {
+self.oninstall = (event) => {
+	caches.open(CACHE).then((cache) => {
+		return cache.addAll(['BTS.mp4', 'BTS-Beispiel.mp4', 'BTS-Beispiel3.mp4',
+		                     'Musikvideos.mp4', 'Narrativ.mp4', 'Showreel.mp4',
+		                     'Unternehmensfilm.mp4', 'index.html',
+		                     'manifest.webmanifest', 'platzhalter.png']);
+	});
 };
 self.fetch((event) => {
 	let request = event.request;
 	event.respondWith(caches.match(request).then((response) => {
-		return response ? response : fetch(request).then((event) => {
+		return response || fetch(request).then((event) => {
 			if(! response || response.status !== 200 || response.type !==
 			   'basic') return response;
 			caches.open(CACHE).then((cache) => {
